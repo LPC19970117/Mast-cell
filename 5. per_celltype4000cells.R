cdbe8662b6c7888be6c497b2ffa809727f51d1b7
LPC19970117 <101658015+LPC19970117@users.noreply.github.com>
2023-10-06 16:52:26 +0800

    Update 5. per_celltype4000cells.R

--- 5. per_celltype4000cells.R
+++ 5. per_celltype4000cells.R
@@ -13,7 +13,7 @@ if(!dir.exists(sam.name)){
 
 load(file = 'sce1.RData')
 
-# 每个细胞亚群抽50 
+# 每个细胞亚群抽50 # Take 50 samples from each cell subpopulation
 sce= SetIdent(sce,value="celltype")
 allCells=names(Idents(sce))
 allType = levels(Idents(sce))
@@ -25,9 +25,9 @@ choose_Cells = unlist(lapply(allType, function(x){
 cg_sce = sce[, allCells %in% choose_Cells]
 cg_sce
 as.data.frame(table(Idents(cg_sce)))
-#输出meta.data数据，在excel中修改后补充
+#输出meta.data数据，在excel中修改后补充#Output meta.data data, modify it in excel and add it
 write.table(cg_sce@meta.data,file=paste0("./",sam.name,"/","cg_sce@meta.data",".txt"),sep="\t",quote = F)
 
 sce<-cg_sce
-#4.存储数据大类sce1
+#4.存储数据大类sce1#4. Storage data category sce1
 save(sce,file=paste0("./",sam.name,"/","sce抽样.RData"))
