7008a3294d5e00fb8b79bd4e6c09e856c25f0792
LPC19970117 <101658015+LPC19970117@users.noreply.github.com>
2023-10-06 14:44:56 +0800

    Update 2. Figure_2e.R

--- 2. Figure_2e.R
+++ 2. Figure_2e.R
@@ -16,7 +16,7 @@ load(file = 'sce_Mast.RData')
 #N and T mast data
 sce_Mast<-subset(sce_Mast,tissue %in% c("Normal","Tumor"))
 
-## sce_Mast重新聚类分组
+## sce_Mas Recluster
 sce_Mast <- NormalizeData(sce_Mast, normalization.method = "LogNormalize", scale.factor = 10000) 
 sce_Mast <- FindVariableFeatures(sce_Mast, selection.method = 'vst', nfeatures = 2000)
 #all.genes <- rownames(sce) 
@@ -29,6 +29,7 @@ sce_Mast<- RunPCA(sce_Mast, features = VariableFeatures(object = sce_Mast))
 #pdf(paste0("./",sam.name,"/PCA-ElbowPlot.pdf"),width = 5,height = 4)
 ElbowPlot(sce_Mast, reduction = "pca",ndims = 50)
 #dev.off()
+
 dim.use<-1:50
 sce_Mast <- FindNeighbors(sce_Mast, dims = 1:50)
 sce_Mast <- FindClusters(sce_Mast, resolution = 0.8 )
@@ -39,11 +40,11 @@ dev.off()
 pdf(paste0("./",sam.name,"/sce_Mast_umap_split_tissue",max(dim.use),"PC.pdf"),width = 8,height = 4)
 DimPlot(sce_Mast, split.by = "tissue",reduction = 'umap',label=T)
 dev.off()
-#### 2.1 计算mast_marker基因
+#### 2.1 calculate mast_marker gene
 all.markers <- FindAllMarkers(sce_Mast, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 write.table(all.markers,file=paste0("./",sam.name,"/","sce_Mast_marker_genes","PC.txt"),sep="\t",quote = F)
 
-#Mast_NormalvsTumor基因的差异
+#Mast_NormalvsTumor gene difference
 sce_Mast= SetIdent(sce_Mast,value="tissue") 
 markers <- FindMarkers(sce_Mast, ident.1="Normal", ident.2="Tumor",
                        assay = 'RNA',slot = 'counts',
@@ -54,7 +55,7 @@ markers <- FindMarkers(sce_Mast, ident.1="Normal", ident.2="Tumor",
                        logfc.threshold =0,min.pct = 0 )
 write.table(markers,file=paste0("./",sam.name,"/","Mast_NormalvsTumor-2",max(dim.use),"PC.txt"),sep="\t",quote = F)
 
-## 气泡图
+## Dot Plot
 features <- c("IL4","IL5","IL9","IL13","IL18",
               "CCL1",
               #"TNF","CSF2","IFNG","KITLG",
@@ -87,7 +88,8 @@ FeaturePlot(sce_Mast, features = features,
             cols = c("#39489f","#39bbec","#f9ed36","#f38466","#b81f25") ,
             reduction = "umap")
 dev.off()
-#细胞类群相似树
+
+#ClusterTree
 sce <- BuildClusterTree(
   sce_Mast,
   dims = dim.use,
@@ -102,7 +104,7 @@ dev.off()
 pdf(paste0("./",sam.name,"/umap_patient.",max(dim.use),"PC.pdf"),width = 8,height = 4)
 DimPlot(object = sce, group.by="patient",reduction='umap')
 dev.off()
-######################热图
+######################heatmap
 sce_Mast= SetIdent(sce_Mast,value="tissue") 
 heatmap_AveE <- AverageExpression(sce_Mast, assays = "RNA", features = features,verbose = TRUE) %>% .$RNA
 
@@ -113,7 +115,7 @@ gaps_row <- cumsum(gene_num)
 
 cluster_num <- c(0)
 gaps_col <- cumsum(cluster_num)
-######################热图
+######################heatmap
 sce_Mast= SetIdent(sce_Mast,value="sample2") 
 heatmap_AveE <- AverageExpression(sce_Mast, assays = "RNA", features = features,verbose = TRUE) %>% .$RNA
 
@@ -147,17 +149,18 @@ pheatmap(heatmap_AveE,cluster_cols = F,cluster_rows = F,show_colnames=T,show_row
          annotation_names_row = F,annotation_names_col = T)
 dev.off()
 
-#输出meta.data数据，在excel中修改后补充
+#Output meta.data data, modify it in excel and add it
 write.table(sce_Mast@meta.data,file=paste0("./",sam.name,"/","sce_mast@meta.data",".txt"),sep="\t",quote = F)
-#补充meta.data数据
+#add meta.data data
 ldf=read.table("sample2.txt",header = T)[,1]
 sce_Mast@meta.data$sample2 <- ldf
 
-#输出heatmap_AveE数据，在excel中排序后输入
+#输出heatmap_AveE数据，在excel中排序后输入#Output heatmap_AveE data, sort it in excel and then input back
 write.table(heatmap_AveE,file=paste0("./",sam.name,"/","heatmap_AveE",".txt"),sep="\t",quote = F)
 
 heatmap_AveE2 <-read.table("Mast_heatmap_input.txt",header = TRUE)
-#将数据从dataframe转为matrix
+
+#将数据从dataframe转为matrix#Convert data from dataframe to matrix
 heatmap_AveE2 <-data.matrix(heatmap_AveE2)
 gene_num <- c(11,8,6,8)
 gaps_row <- cumsum(gene_num)
